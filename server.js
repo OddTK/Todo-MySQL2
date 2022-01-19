d614c8cfbc297e90d3faac1caaf982a5d18566e9
Jesus <jesusbalandra@outlook.com>
2022-01-18 21:30:58 -0700

    added to code

--- server.js
+++ server.js
@@ -9,6 +9,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// USER API's
+app.post('/api/users', async (req, res) => {
+    const [username ] = req.body;
+    if (!username) {
+        return res.status(400).json({ error: 'You must provide a username' });
+    }
+    try {
+        const createUserQuery = 'INSERT INTO users(username) VALUES(?);';
+        const getUserByIdQuery = 'SELECT * FROM users WHERE id = ?;';
+        const [result] = await connection.query(createUserQuery, [username]);
+        const [userResult] = await connection.query(getUserByIdQuery, [result.insertId]);
+        res.json(userResult[0]);
+    } catch (e) {
+        res.json(400).json(e);
+    }
+});
+
+app.get('/api/todos', async (req, res) => {
+    try {
+        const getAllTodosQuery = 'SELECT * FROM todos;';
+        const [ todos ] = await connection.query(getAllTodosQuery);
+        res.json(todos);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+});
+
 // Declaring a function as async allows us to use await syntax inside of that function
 app.post('/api/todos', async (req, res) => {
     // { task: 'Sleep' } - destructuring task
